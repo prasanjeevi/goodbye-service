a8545d2d5a059cf56d3a97c03ca8458c4678a643
Sanjeevi <prasanjeevi@gmail.com>
2017-08-07 11:53:15 +0530

    spring init

---
+++ src/main/java/com/cloudier/goodbyeservice/GoodbyeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cloudier.goodbyeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbyeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyeServiceApplication.class, args);
+	}
+}
