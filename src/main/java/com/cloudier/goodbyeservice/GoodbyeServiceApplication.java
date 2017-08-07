3101baee50ebfa12cd82d685bbb2865fef968143
Sanjeevi <prasanjeevi@gmail.com>
2017-08-07 12:19:19 +0530

    basic service 2

--- src/main/java/com/cloudier/goodbyeservice/GoodbyeServiceApplication.java
+++ src/main/java/com/cloudier/goodbyeservice/GoodbyeServiceApplication.java
@@ -2,11 +2,21 @@ package com.cloudier.goodbyeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class GoodbyeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoodbyeServiceApplication.class, args);
+	}
+	
+	@RequestMapping
+	public String goodbye() {
+		return "Goodbye!";
 	}
 }
